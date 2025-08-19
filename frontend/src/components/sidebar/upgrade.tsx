02b3f8d16becd0a5d83838878c19c96d62e54eeb
RafayN-7 <naumanrafay7@gmail.com>
2025-08-19 20:17:12 +0500

    updated

--- frontend/src/components/sidebar/upgrade.tsx
+++ frontend/src/components/sidebar/upgrade.tsx
@@ -1,9 +1,20 @@
 "use client"
 
+import { authClient } from "~/lib/auth-client"
 import { Button } from "../ui/button"
 
 export default function Upgrade() {
+    const upgrade = async () => {
+        await authClient.checkout({
+            products: [
+                "f52d7d14-7248-4a6e-bd0a-7e5578af79e3",
+                "4a9ab1fa-7297-419e-9017-ff06117d4770",
+                "cba37f79-cb61-41d0-a7d9-8845248fbb96",
+            ],
+        });
+    };
 return <Button variant="outline" size="sm" className="ml-2 cursor-pointer text-orange-400"
+onClick={upgrade}
 
 >
 Upgrade
--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,13 +1,89 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "~/server/db";
+import { Polar } from "@polar-sh/sdk";
+import { env } from "~/env";
+import {
+  polar,
+  checkout,
+  portal,
+  usage,
+  webhooks,
+} from "@polar-sh/better-auth";
 
- 
+const polarClient = new Polar({
+  accessToken: env.POLAR_ACCESS_TOKEN,
+  server: "sandbox",
+});
+
 export const auth = betterAuth({
-    database: prismaAdapter(db, {
-        provider: "postgresql", // or "mysql", "postgresql", ...etc
+  database: prismaAdapter(db, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "f52d7d14-7248-4a6e-bd0a-7e5578af79e3",
+              slug: "small",
+            },
+            {
+              productId: "4a9ab1fa-7297-419e-9017-ff06117d4770",
+              slug: "medium",
+            },
+            {
+              productId: "cba37f79-cb61-41d0-a7d9-8845248fbb96",
+              slug: "large",
+            },
+          ],
+          successUrl: "/",
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+        webhooks({
+          secret: env.POLAR_WEBHOOK_SECRET,
+          onOrderPaid: async (order) => {
+            const externalCustomerId = order.data.customer.externalId;
+
+            if (!externalCustomerId) {
+              console.error("No external customer ID found.");
+              throw new Error("No external customer id found.");
+            }
+
+            const productId = order.data.productId;
+
+            let creditsToAdd = 0;
+
+            switch (productId) {
+              case "f52d7d14-7248-4a6e-bd0a-7e5578af79e3":
+                creditsToAdd = 10;
+                break;
+              case "4a9ab1fa-7297-419e-9017-ff06117d4770":
+                creditsToAdd = 25;
+                break;
+              case "cba37f79-cb61-41d0-a7d9-8845248fbb96":
+                creditsToAdd = 50;
+                break;
+            }
+
+            await db.user.update({
+              where: { id: externalCustomerId },
+              data: {
+                credits: {
+                  increment: creditsToAdd,
+                },
+              },
+            });
+          },
+        }),
+      ],
     }),
-    emailAndPassword: {
-    enabled: true, 
-  }, 
+  ],
 });
